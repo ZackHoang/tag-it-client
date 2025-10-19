4b4b74987efa42ec1dc39402c52d27586d5901d3
Zack <zackhoang2004@gmail.com>
2025-10-19 10:28:49 -0700

    fix: pop up box now appears correctly

--- src/pages/game/Game.jsx
+++ src/pages/game/Game.jsx
@@ -1,28 +1,30 @@
 import { useParams } from "react-router-dom";
 import styles from "./game.module.css";
 import data from "../../data";
-import {  useState } from "react";
+import { useRef, useState } from "react";
 
 export default function Game() {
   const { game } = useParams();
   const [click, setClick] = useState(false);
+  const imageRef = useRef(null);
   const [mousePosition, setMousePosition] = useState({
     x: null,
-    y: null
+    y: null,
   });
   const gameIndex = parseInt(game[game.length - 1]) - 1;
 
   const handleModal = async (e) => {
-    setClick(!click);
-    setMousePosition({
-      x: e.clientX, 
-      y: e.clientY
-    })
+    const mousePosition = {
+      x: e.nativeEvent.offsetX,
+      y: e.nativeEvent.offsetY,
+    }
     console.log(mousePosition);
+    setClick(!click);
+    setMousePosition(mousePosition);
   };
 
   return (
-    <>
+    <div id={styles.game}>
       <div className={styles.targets}>
         {data[gameIndex].targets.map((char, index) => {
           return (
@@ -33,19 +35,22 @@ export default function Game() {
           );
         })}
       </div>
-      <img
-        className={styles.mainImage}
-        src={data[gameIndex].image}
-        onMouseDown={handleModal}
-      ></img>
-      {click && (
-        <div
-          style={{ left: `${mousePosition.x}px`, top: `${mousePosition.y}px` }}
-          className={styles.popUp}
-        >
-          Mouse is clicked!
-        </div>
-      )}
-    </>
+      <div id={styles.imageContainer}>
+        <img
+          className={styles.mainImage}
+          src={data[gameIndex].image}
+          ref={imageRef}
+          onMouseDown={handleModal}
+        ></img>
+        {click && (
+          <div
+            style={{ left: mousePosition.x, top: mousePosition.y }}
+            className={styles.popUp}
+          >
+            Mouse is clicked!
+          </div>
+        )}
+      </div>
+    </div>
   );
 }
