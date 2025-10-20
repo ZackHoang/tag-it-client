a35d53d0b82935208fca4ed32702d33cae212304
Zack <zackhoang2004@gmail.com>
2025-10-20 14:26:29 -0700

    fix: add mock

--- src/tests/App.test.jsx
+++ src/tests/App.test.jsx
@@ -1,14 +1,28 @@
-import { describe, it, expect } from "vitest";
-import { screen } from "@testing-library/react";
+import { describe, it, expect, vi } from "vitest";
+import { render, screen } from "@testing-library/react";
 import renderWithRouter from "./util";
+import Game from "../pages/game/Game";
+import userEvent from "@testing-library/user-event";
+
+vi.mock("../pages/game/Game", () => ({ handlePopUp }) => {
+  <img
+    alt="main game image"
+    src="https://res.cloudinary.com/dazy1wrx0/image/upload/pixelcon_a10dsa.jpg"
+    onMouseDown={handlePopUp}
+  ></img>;
+});
 
 describe("App component", () => {
   it("renders correct header and footer", () => {
     renderWithRouter();
-    expect(screen.getByRole("heading", {name: "TagIt"})).toBeInTheDocument();
-    expect(screen.getByRole("link", {name: "Home"})).toBeInTheDocument();
-    expect(screen.getByRole("link", {name: "Leaderboard"})).toBeInTheDocument();
-    expect(screen.getByRole("heading", {name: "by Zack Hoang"})).toBeInTheDocument();
+    expect(screen.getByRole("heading", { name: "TagIt" })).toBeInTheDocument();
+    expect(screen.getByRole("link", { name: "Home" })).toBeInTheDocument();
+    expect(
+      screen.getByRole("link", { name: "Leaderboard" })
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole("heading", { name: "by Zack Hoang" })
+    ).toBeInTheDocument();
   });
 });
 
@@ -17,21 +31,26 @@ describe("Header component", () => {
     const { user } = renderWithRouter();
     const homeButton = screen.getByRole("link", { name: "Home" });
     await user.click(homeButton);
-    expect(screen.getByRole("link", {name: "Play"})).toBeInTheDocument();
-    expect(screen.getByRole("link", {name: "Source"})).toBeInTheDocument();
+    expect(screen.getByRole("link", { name: "Play" })).toBeInTheDocument();
+    expect(screen.getByRole("link", { name: "Source" })).toBeInTheDocument();
   });
 });
 
 describe("Home page", () => {
   it("game renders all necessary components", async () => {
     const { user } = renderWithRouter();
-    const button = screen.getAllByRole("link", {name: "Play"})[0];
+    const button = screen.getAllByRole("link", { name: "Play" })[0];
     await user.click(button);
     expect(screen.getAllByRole("figure").length).toBe(3);
     expect(screen.getByAltText("main game image")).toBeInTheDocument();
   });
 
-  it("user's click coordinates should return (0, 0) on top left of main game image", () => {
-    //???
-  })
+  it("call handlePopUp when clicked on image", async () => {
+    const handlePopUp = vi.fn();
+    const user = userEvent.setup();
+    render(<Game handlePopUp={handlePopUp}></Game>);
+    const image = screen.getByAltText("main game image");
+    await user.click(image);
+    expect(handlePopUp).toHaveBeenCalled();
+  });
 });
